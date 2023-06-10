9032bf6d0469b1d4ba2d420c5c31cb957c5f645b
Girjesh786 <95648184+Girjesh786@users.noreply.github.com>
2023-06-10 22:09:29 +0530

    Update employee.java

--- devopsnew/src/org/java/employee.java
+++ devopsnew/src/org/java/employee.java
@@ -1,5 +1,16 @@
 package org.java;
 
 public class employee {
-
+public int addemp() {
+		int emp=101;
+		System.out.println("add"+emp);
+		return emp;
+	}
+	public void updateemployee() {
+		System.out.println("Updated the employee details");
+	}
+	public static void main(String[] args) {
+	employee emp = new  employee();
+	emp.addemp();
+	emp.updateemployee();	}
 }
